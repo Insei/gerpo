e30f25d9f0c32c91c8ec76b0d3cf36a83ad21a24
ymiad <ymiadzelets@gmail.com>
2024-12-11 10:30:32 +0300

    fixed offset calc alg

--- query/linq/pagination.go
+++ query/linq/pagination.go
@@ -6,29 +6,34 @@ type Limiter interface {
 }
 
 type PaginationBuilder struct {
-	opts []func(b Limiter)
+	opts  []func(b Limiter)
+	limit uint64
+	page  uint64
 }
 
 func NewPaginationBuilder() *PaginationBuilder {
-	return &PaginationBuilder{}
+	return &PaginationBuilder{
+		limit: 1,
+		page:  0,
+	}
 }
 
 func (q *PaginationBuilder) Page(page uint64) {
-	q.opts = append(q.opts, func(b Limiter) {
-		if page > 0 {
-			page--
-		}
-		b.Offset(page)
-	})
+	if page > 0 {
+		page--
+	}
+	q.page = page
 }
 
 func (q *PaginationBuilder) Size(size uint64) {
-	q.opts = append(q.opts, func(b Limiter) {
-		b.Limit(size)
-	})
+	q.limit = size
 }
 
 func (q *PaginationBuilder) Apply(paginator Limiter) {
+	q.opts = append(q.opts, func(b Limiter) {
+		b.Limit(q.limit)
+		b.Offset(q.page * q.limit)
+	})
 	for _, opt := range q.opts {
 		opt(paginator)
 	}
