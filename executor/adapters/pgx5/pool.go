a38b8ba6605a3cab20dc89ace0d899d6552b5f7a
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2025-04-20 00:02:28 +0300

    src: executor: adapters: add pgx v5 adapter
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

---
+++ executor/adapters/pgx5/pool.go
@@ -0,0 +1,54 @@
+package pgx4
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/insei/gerpo/executor/adapters/placeholder"
+	"github.com/insei/gerpo/executor/types"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type poolWrap struct {
+	pool *pgxpool.Pool
+}
+
+func (p *poolWrap) ExecContext(ctx context.Context, query string, args ...any) (types.Result, error) {
+	sql, err := placeholder.Dollar.ReplacePlaceholders(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to replace placeholders: %w", err)
+	}
+	res, err := p.pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	return &resultWrap{res: res}, nil
+}
+
+func (p *poolWrap) QueryContext(ctx context.Context, query string, args ...any) (types.Rows, error) {
+	sql, err := placeholder.Dollar.ReplacePlaceholders(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to replace placeholders: %w", err)
+	}
+	rows, err := p.pool.Query(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	return &rowsWrap{rows: rows}, nil
+}
+
+func (p *poolWrap) BeginTx(ctx context.Context) (types.Tx, error) {
+	tx, err := p.pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return &txWrap{
+		rollbackUnlessCommittedNeeded: true,
+		tx:                            tx,
+	}, err
+}
+
+func NewPoolAdapter(pool *pgxpool.Pool) types.DBAdapter {
+	return &poolWrap{pool}
+}
---
+++ executor/adapters/pgx5/rows.go
@@ -0,0 +1,22 @@
+package pgx4
+
+import (
+	"github.com/jackc/pgx/v5"
+)
+
+type rowsWrap struct {
+	rows pgx.Rows
+}
+
+func (r *rowsWrap) Next() bool {
+	return r.rows.Next()
+}
+
+func (r *rowsWrap) Scan(dest ...interface{}) error {
+	return r.rows.Scan(dest...)
+}
+
+func (r *rowsWrap) Close() error {
+	r.rows.Close()
+	return nil
+}
