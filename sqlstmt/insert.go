504ff4f58b8904dbbdc6cd542ec948f1c59cec98
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2026-04-19 03:04:33 +0300

    chore: drop unused private sql() helpers in sqlstmt

--- sqlstmt/insert.go
+++ sqlstmt/insert.go
@@ -2,7 +2,6 @@ package sqlstmt
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
 	"github.com/insei/gerpo/types"
@@ -28,32 +27,6 @@ func NewInsert(ctx context.Context, table string, colStorage types.ColumnsStorag
 		vals:  newValues(columns),
 		table: table,
 	}
-}
-
-func (i *Insert) sql() (string, error) {
-	if i.table == "" {
-		return "", ErrTableIsNoSet
-	}
-	cols := i.columns.GetAll()
-	if len(cols) < 1 {
-		return "", ErrEmptyColumnsInExecutionSet
-	}
-	sqlTemplate := "(%s) VALUES (%s)"
-	colsStr := ""
-	valuesCount := 0
-	for _, col := range cols {
-		colName, ok := col.Name()
-		if !ok {
-			continue
-		}
-		if colsStr != "" {
-			colsStr += ", "
-		}
-		colsStr += colName
-		valuesCount++
-	}
-	valuesSQLTemplate := strings.Repeat("?,", valuesCount)
-	return fmt.Sprintf("INSERT INTO %s "+sqlTemplate, i.table, colsStr, valuesSQLTemplate[:len(valuesSQLTemplate)-1]), nil
 }
 
 func (i *Insert) Columns() types.ExecutionColumns {
